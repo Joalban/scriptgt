938ea2b44eba3658803eebe5f91c7ed9fb266335
Joalban <131504228+Joalban@users.noreply.github.com>
2023-05-13 15:37:47 +0200

    Update gtnd.js

--- notepad/gtnd.js
+++ notepad/gtnd.js
@@ -1,4 +1,4 @@
 ScriptAPI.register( 'GT Notepad v 1.0', 8.3, 'Black Dragon', 'tw@hemispheria.net' );
 /*  Javascript:$.getScript("http://scriptgt.125mb.com/gt/notepad/gtnd.js"); void(0);*/
 
-javascript: var pop_up = window.open("http://scriptgt.125mb.com/gt/notepad/notepad.html", "GT Notepad", "width=700, height=300"); void(0);
+javascript: var pop_up = window.open("https://github.com/Joalban/scriptgt/blob/main/notepad/notepad.html", "GT Notepad", "width=700, height=300"); void(0);
