bd433f5204314456960e5c2eed6eb99337025617
Joalban <131504228+Joalban@users.noreply.github.com>
2023-05-14 02:47:02 +0200

    Add files via upload

---
+++ tropas/addunit.js
@@ -0,0 +1,16 @@
+javascript: 
+var doc=document;   
+insertUnit(doc.forms[0].spear, 1);
+insertUnit(doc.forms[0].sword, 1);    
+insertUnit(doc.forms[0].axe, 1);   
+insertUnit(doc.forms[0].archer, 1);
+insertUnit(doc.forms[0].spy, 1);  
+insertUnit(doc.forms[0].light, 1); 
+insertUnit(doc.forms[0].marcher, 1);  
+insertUnit(doc.forms[0].heavy, 1); 
+insertUnit(doc.forms[0].ram, 1);   
+insertUnit(doc.forms[0].catapult, 1);
+insertUnit(doc.forms[0].knight, 1);  
+insertUnit(doc.forms[0].snob, 1);
+{document.forms[0].submit.click();
+void(0);
