7dff5ac67ab877b7dbb363eceb6ea72b3008724b
Joalban <131504228+Joalban@users.noreply.github.com>
2023-05-14 02:50:30 +0200

    Update addunit.js

--- tropas/addunit.js
+++ tropas/addunit.js
@@ -1,15 +1,15 @@
 javascript: 
 var doc=document;   
-insertUnit(doc.forms[0].spear, 1);
-insertUnit(doc.forms[0].sword, 1);    
-insertUnit(doc.forms[0].axe, 1);   
-insertUnit(doc.forms[0].archer, 1);
-insertUnit(doc.forms[0].spy, 1);  
-insertUnit(doc.forms[0].light, 1); 
-insertUnit(doc.forms[0].marcher, 1);  
-insertUnit(doc.forms[0].heavy, 1); 
-insertUnit(doc.forms[0].ram, 1);   
-insertUnit(doc.forms[0].catapult, 1);
-insertUnit(doc.forms[0].knight, 1);  
-insertUnit(doc.forms[0].snob, 1);
+insertUnit(doc.forms[0].spear, 5);
+insertUnit(doc.forms[0].sword, 5);    
+insertUnit(doc.forms[0].axe, 5);   
+insertUnit(doc.forms[0].archer, 5);
+insertUnit(doc.forms[0].spy, 5);  
+insertUnit(doc.forms[0].light, 5); 
+insertUnit(doc.forms[0].marcher, 5);  
+insertUnit(doc.forms[0].heavy, 5); 
+insertUnit(doc.forms[0].ram, 5);   
+insertUnit(doc.forms[0].catapult, 5);
+insertUnit(doc.forms[0].knight, 5);  
+insertUnit(doc.forms[0].snob,5);
 void(0);
